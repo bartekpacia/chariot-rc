93dcc0afa62f264aab22f234b4c31bda1df231f9
Bartek Pacia <barpac02@gmail.com>
2020-08-06 01:30:13 +0200

    add more Go code

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.14
 
 require (
 	cloud.google.com/go/firestore v1.3.0 // indirect
-	firebase.google.com/go v3.13.0+incompatible // indirect
-	google.golang.org/api v0.30.0 // indirect
+	firebase.google.com/go v3.13.0+incompatible
+	google.golang.org/api v0.30.0
 )
--- pilot.go
+++ pilot.go
@@ -1,7 +1,30 @@
 package main
 
-import ("fmt")
+import (
+	"context"
+	"firebase.google.com/go"
+	"google.golang.org/api/option"
+	"log"
+)
 
-func main()  {
-	fmt.Println("Hi");
+type Data struct {
+        Engine int `json:"engine,omitempty"`
+}
+
+func main() {
+	sa := option.WithCredentialsFile("./key.json")
+	app, err := firebase.NewApp(context.Background(), nil, sa)
+	if err != nil {
+		log.Fatalf("Error initializing Firebase app")
+	}
+
+	client, err := app.Database(context.Background())
+	if err != nil {
+		log.Fatalf("Error initializing RTDB")
+	}
+
+	ref := client.NewRef("chariot-rc").Child("chariot_1")
+
+	var data Data
+	err := ref.Get(context.Background(), &data)
 }
