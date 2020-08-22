5c8788448dfd77b4daca5339fa140e9cd34f42b5
Bartek Pacia <barpac02@gmail.com>
2020-08-22 21:41:33 +0200

    change ports

--- client.js
+++ client.js
@@ -11,8 +11,10 @@ let GREEN
 let SERVO
 
 board.on("ready", () => {
-  const led = new five.Led("P1-13")
-  led.blink()
+  const led8 = new five.Led(8)
+  const led14 = new five.Led(14)
+  led8.blink()
+  led14.blink()
 })
 
 admin.initializeApp({
